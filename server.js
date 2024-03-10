feec6f94d26c8fd260751388129b67d1b75daec5
laiso <aloisleroy2016@gmail.com>
2024-03-10 16:57:48 +0100

    Server file

---
+++ server.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+
+// Initialize the database
+require('./database/mongo.conf.js').init();
+
+/*
+    Create the express server
+*/
+const express = require('express'),
+      app = express(),
+      port = process.env.PORT || 8080,
+      domain = process.env.DOMAIN || 'localhost';
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.use("/api/auth/", require('./routes/register.js'));
+app.use("/api/auth/", require('./routes/login.js'));
+
+app.get("/protected", require('./middleware/isLoggedIn.js'), (req, res) => {
+    res.send('Protected route');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://${domain}:${port}`);
+});
